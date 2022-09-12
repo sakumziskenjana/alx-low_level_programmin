53ec3ab8debfe8d554cfe12d368ffec27f500bea
Sakumzi <skenajnakeith@gmail.com>
2022-09-12 10:28:30 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low --)
+		putchar (low);
+	putchar ('\n');
+
+	return (0);
+}

eb495187e3fc5362e2affe93a6d84cc0044a07cc
Sakumzi <skenajnakeith@gmail.com>
2022-09-13 14:08:06 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,17 @@
  *
  * Return: 0
  */
-int main(void)
+void print_alphabet_x10(void);
 {
-	print_alphabet_x10();
-	return(0);
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for(j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }

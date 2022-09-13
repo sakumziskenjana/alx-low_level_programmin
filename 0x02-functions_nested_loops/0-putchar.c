19daedbf9a96675ef79468545b24802bff3cf18a
Sakumzi <skenajnakeith@gmail.com>
2022-09-13 12:23:57 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - print putchar
  *
  * Return: 0
  */
@@ -14,7 +14,7 @@ int main(void)
 	_putchar("h");
 	_putchar("a");
 	_putchar("r");
-	_putchar("\n");
+	_putchar('\n');
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#infdef HOLDBERTON_N_INCLUDED
+#define HOLDBERTON_N_INCLUDED
+
+int_putchar(char);
+void_print_alphabet(void);
+void_print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+itn _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif

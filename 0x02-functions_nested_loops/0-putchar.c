34aa6fd346af65dd9e1fd39f18902a92120003c9
Sakumzi <skenajnakeith@gmail.com>
2022-09-13 13:18:58 -0700

    Taask 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <sdlib.h>
+#include <time.h>
 /**
  * main - entry point
- *Description: a program that print _putchar
+ * Description: a program that print _putchar
  * Return: 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 
 	return (0);
 }

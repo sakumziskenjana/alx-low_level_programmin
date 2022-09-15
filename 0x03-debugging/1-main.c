77ef446adb2806f95d598d487a64ecfc1b3a2fb1
Sakumzi <skenajnakeith@gmail.com>
2022-09-15 13:35:09 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,14 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-			i = 0;
-			/*
-			 * while (i < 10)
-			 * {
-			 * putchar(i);
-			 * }
-			 */
-			printf("Infinite loop evaded!\\o/\n");
+	i = 0;
 
-			return (0);
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }

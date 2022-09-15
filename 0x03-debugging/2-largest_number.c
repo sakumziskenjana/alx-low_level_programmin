fdb910d4b1864efa87d2bf7f9e099f5318e98f10
Sakumzi <skenajnakeith@gmail.com>
2022-09-15 13:37:48 -0700

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - largest_number - returns the largest of three numbers
+ * main - largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer

c4f9c3b17e7cac0d48b92e84171cbdfa6c866eae
Sakumzi <skenajnakeith@gmail.com>
2022-09-20 14:02:16 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,26 +9,18 @@
 
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	int l, i;
+	char ch;
 
-	a = s;
+	/*find string length without null char*/
+	for (l = 0; s[l] != '\0'; ++l)
+		;
 
-	while (s[c] != '\0')
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; ++i)
 	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
+		 ch = s[i];
+		 s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		 s[l - 1 - i] = ch;
 	}
 }

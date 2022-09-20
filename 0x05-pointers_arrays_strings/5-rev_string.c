10d313ea28dd335a3b4bdd22bbc7e28ac02d5d12
Sakumzi <skenajnakeith@gmail.com>
2022-09-20 14:05:20 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,11 +12,9 @@ void rev_string(char *s)
 	int l, i;
 	char ch;
 
-	/*find string length without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
 		;
 
-	/*swap the string by looping to half the string*/
 	for (i = 0; i < l / 2; ++i)
 	{
 		 ch = s[i];

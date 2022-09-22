8117c8263263bd7c2702c1ee35249df089eea304
Sakumzi <skenajnakeith@gmail.com>
2022-09-22 01:03:48 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The lomit of the concatenation
+ *
+ * Return: A ponter to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0, j = 0;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	while (j < n && src[j]);
+	{
+		dest[dlen] = src[j];
+		dlen++;
+		j++;
+	}
+
+	dest[dlen + n + 1] = '\0';
+
+	return (dest);
+}

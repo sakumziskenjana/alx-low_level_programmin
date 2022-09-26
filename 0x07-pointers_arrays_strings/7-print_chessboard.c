79d2eb50f19d046df83255ba4146d4b163fb6ccf
Sakumzi <skenajnakeith@gmail.com>
2022-09-26 14:24:19 -0700

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,13 +10,11 @@ void print_chessboard(char (*a)[8])
 {
 	int b, c;
 
-	for (b = 0; b < 8; b++)
+	for (b = 0; a[b][8]; b++)
 	{
 		for (c = 0; c < 8; c++)
-		{
 			_putchar(a[b][c]);
-		}
 
 		 _putchar('\n');
-}
+	}
 }

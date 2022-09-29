0e0b41c7e72843f8aa7d3299af8617691e7de004
Sakumzi <skenajnakeith@gmail.com>
2022-09-28 19:31:43 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s + 1);
 
 }
